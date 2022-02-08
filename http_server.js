172add954f97cf8120aa586af331c875f02374f8
Carmen Constantinescu <87343796+ciconstantinescu@users.noreply.github.com>
2022-02-08 10:02:38 -0500

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -47,9 +47,10 @@ app.post('/add', function(req, res){
 });
 
 // start server
-// -----------------------
-app.listen(3001, function(){
-    console.log('Running on port 3001!')
-})
 
 app.listen(process.env.PORT || 3000);
+// -----------------------
+//app.listen(3001, function(){
+    //console.log('Running on port 3001!')
+})
+
